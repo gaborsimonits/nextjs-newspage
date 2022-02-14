adf7c737b68efa17bb1847334c9a3e498971eff7
gaborsimonits <gaborsimonits@gmail.com>
2022-02-14 16:02:11 +0100

    push fetched data in card component

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,15 @@
 import cardStyles from "../styles/Card.module.css";
+import Image from "next/image";
 
 const Card = ({ news }) => {
 	return (
 		<div className={cardStyles.newsbox}>
-			{/* <img className={cardStyles.newsimage} src={news.urlToImage} alt='' /> */}
+			{/* <Image
+				src={news.urlToImage}
+				alt='news article photo'
+				className={cardStyles.newsimage}
+			/> */}
+			{/* <img className={cardStyles.newsimage} src={news.urlToImage} /> */}
 			<h1>{news.title}</h1>
 			{/* <p dangerouslySetInnerHTML={{__html: news.description}}></p> */}
 			<a className={cardStyles.readmore} href={news.url}>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: [
+			"https://newsapi.org/v2/top-headlines?country=us&apiKey=7593c7116e6b46288c890afce6ab9d3f",
+		],
+	},
+};
